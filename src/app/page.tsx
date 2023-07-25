86a5df4279d970b9bb7cb0465e09c8d39b7ee0e5
Mark Njoroge <marknjo7@gmail.com>
2023-07-25 14:24:25 +0300

    add basic structure to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Blog</h1>
+    <main className='px-6 mx-auto'>
+      <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>
+        Hello and welcome 👋&nbsp;
+        <span className='whitespace-nowrap'>
+          I'm <span className='font-bold'>Mark</span>
+        </span>
+      </p>
     </main>
   );
 }
