75558240ebdc7cadd2c29650031601bba127ef60
Mark Njoroge <marknjo7@gmail.com>
2023-07-28 15:20:44 +0300

    add Icons to the NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,43 @@
 import Link from 'next/link';
 
+import { FaYoutube, FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const icons: Array<LinkIconProps> = [
+  {
+    url: 'https://youtube.com/@thetalkingdev',
+    icon: <FaYoutube />,
+  },
+
+  {
+    url: 'https://www.linkedin.com/in/mark-njoroge/',
+    icon: <FaLinkedinIn />,
+  },
+  {
+    url: 'https://github.com/Marknjo',
+    icon: <FaGithub />,
+  },
+  {
+    url: 'https://twitter.com/marknjo7',
+    icon: <FaTwitter />,
+  },
+];
+
+type LinkIconProps = {
+  url: string;
+  icon: JSX.Element;
+};
+
+function LinkIcon({ icon, url }: LinkIconProps) {
+  return (
+    <Link
+      target='_blank'
+      href={url}
+      className='text-white/90 hover:text-white transition rounded-full w-8 h-8 sm:w-9 sm:h-9 bg-slate-800 hover:bg-slate-500 flex justify-center items-center'>
+      {icon}
+    </Link>
+  );
+}
+
 export default function Navbar() {
   return (
     <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
@@ -11,6 +49,13 @@ export default function Navbar() {
             Mark Njoroge
           </Link>
         </h1>
+
+        <div className='flex flex-row' />
+        <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-lg lg:text-2xl'>
+          {icons.map((icon) => (
+            <LinkIcon key={icon.url} {...icon} />
+          ))}
+        </div>
       </div>
     </nav>
   );
