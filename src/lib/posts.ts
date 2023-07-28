f349a9bcd6a7d4ab95e693bcc29d2e3c31c8bf11
Mark Njoroge <marknjo7@gmail.com>
2023-07-28 16:03:26 +0300

    implement getSortedPostsData() server lib method

---
+++ src/lib/posts.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+const postsDir = path.join(process.cwd(), 'posts');
+
+export default function getSortedPostsData() {
+  // get file names unde /posts
+  const fileNames = fs.readdirSync(postsDir);
+  const allPostsData = fileNames.map((filename) => {
+    // remove ".md" from file name to get id
+
+    const id = filename.replace(/\.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDir, filename);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the metadata section
+    const matterResult = matter(fileContents);
+
+    const blogPost: Post = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+    };
+
+    // Combine the date with the id
+    return blogPost;
+  });
+
+  // Sort posts by date
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
