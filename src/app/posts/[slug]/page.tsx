fc931bc990760503cece7ff43d544bff7a644ccc
Mark Njoroge <marknjo7@gmail.com>
2023-07-29 16:56:53 +0300

    structure, style & implement single post page

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,35 @@
+import formatDate from '@/lib/formatDate';
+import getSortedPostsData, { getPostData } from '@/lib/posts';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Post({ params }: Props) {
+  const posts = getSortedPostsData();
+
+  const { slug } = params;
+
+  if (!slug || !posts.find((post) => post.id === slug)) notFound();
+
+  const { title, date, contentHtml } = await getPostData(slug);
+  const pubDate = formatDate(date);
+
+  return (
+    <main className='px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
+      <h1 className='text-3xl mt-4 mb-0'>{title}</h1>
+      <p className='mt-0'>{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+
+        <p>
+          <Link href='/'>&larr; Back to Home</Link>
+        </p>
+      </article>
+    </main>
+  );
+}
