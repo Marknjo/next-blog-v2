5de95862fffd2e5d01b9d9d516784f5ab650a661
Mark Njoroge <marknjo7@gmail.com>
2023-07-29 16:54:29 +0300

    implement getPostData server utility method

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import path from 'path';
+import { remark } from 'remark';
+import html from 'remark-html';
 const postsDir = path.join(process.cwd(), 'src', 'posts');
 
 console.log(postsDir);
@@ -32,4 +34,29 @@ export default function getSortedPostsData() {
 
   // Sort posts by date
   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDir, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+  const blogPostWithHTML: Post & { contentHtml: string } = {
+    id,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    contentHtml,
+  };
+
+  // combine the date with the id
+
+  return blogPostWithHTML;
 }
