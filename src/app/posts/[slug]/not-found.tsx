3f1fa609418bee8c5b3bd391bfa702b400a52fe7
Mark Njoroge <marknjo7@gmail.com>
2023-07-29 17:20:16 +0300

    add basic styles to not-found component - post/[slug]/not-found

--- src/app/posts/[slug]/not-found.tsx
+++ src/app/posts/[slug]/not-found.tsx
@@ -1,3 +1,9 @@
 export default function NotFound() {
-  return <h1>The requested post does not exist in our archives</h1>;
+  return (
+    <main className='px-6 prose prose-xl prose-slate dark:prose-invert mx-auto mt-5'>
+      <h1 className='text-center'>
+        The requested post does not exist in our archives
+      </h1>
+    </main>
+  );
 }
