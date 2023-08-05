2132960b69713cb35a9a8bed1590b6cdf08ccc13
Mark Njoroge <marknjo7@gmail.com>
2023-08-05 18:57:18 +0300

    implement getPostsMeta server lib methoed for fetching all posts from gitHub

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -50,3 +50,36 @@ export async function getPostByName(
 
   return blogPostObj;
 }
+
+export async function getPostsMeta(): Promise<Meta[] | undefined> {
+  const res = await fetch(
+    'http://api.github.com/repos/Marknjo/next-blog-v2-test-posts/git/trees/main?recursive=1',
+    {
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    }
+  );
+
+  if (!res.ok) return undefined;
+
+  const repoFiletree: Filetree = await res.json();
+
+  const filesArray = repoFiletree.tree
+    .map((obj) => obj.path)
+    .filter((path) => path.endsWith('.mdx'));
+
+  const posts: Meta[] = [];
+
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) {
+      const { meta } = post;
+      posts.push(meta);
+    }
+  }
+
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
