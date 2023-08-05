dec44fef6394d5f7b42ca8ffa922770c38eb2621
Mark Njoroge <marknjo7@gmail.com>
2023-08-05 19:05:58 +0300

    refactor /posts/[slug] not-found styles & structure

--- src/app/posts/[slug]/not-found.tsx
+++ src/app/posts/[slug]/not-found.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
-    <main className='px-6 prose prose-xl prose-slate dark:prose-invert mx-auto mt-5'>
-      <h1 className='text-center'>
-        The requested post does not exist in our archives
-      </h1>
-    </main>
+    <div className='text-center'>
+      <p className='mt-10'>The requested post does not exist in our archives</p>
+      <Link href='/'>&larr; Back to Home</Link>
+    </div>
   );
 }
