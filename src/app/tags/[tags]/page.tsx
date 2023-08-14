848fd484178d7035aacdc38a012d3f4d55e5625c
Mark Njoroge <marknjo7@gmail.com>
2023-08-14 08:06:07 +0300

    add tags/[tag] page and implement generateStaticParams() method for tags

---
+++ src/app/tags/[tags]/page.tsx
@@ -0,0 +1,21 @@
+import { getPostsMeta } from '@/lib/posts';
+
+export const revalidate = 0; //86400
+
+type Props = {
+  params: {
+    tag: string;
+  };
+};
+
+export async function generateStaticParams() {
+  const posts = (await getPostsMeta()) || [];
+
+  if (posts.length === 0) return [];
+
+  const tags = new Set(posts.map((post) => post.tags).flat());
+
+  return Array.from(tags).map((tag) => ({
+    tag,
+  }));
+}
